230bfba26b304c5cd8c4585bdf5e6c3fa4a9f52e
hilal aissani <hilalaissani@gmail.com>
2019-02-18 19:46:49 -0500

    all2

--- linkedList.js
+++ linkedList.js
@@ -25,3 +25,46 @@ class LinkedList {
     return;
   }
 }
+
+/**
+ *
+ * You are given two non-empty linked lists representing
+ * two non-negative integers. The digits are stored in
+ * reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
+ *
+ */
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+var addTwoNumbers = function(l1, l2) {
+  let node1 = l1;
+  let node2 = l2;
+  let carry = 0;
+
+  const dummyHead = new ListNode(0);
+  let currentNode = dummyHead;
+
+  while (node1 || node2 || carry > 0) {
+    const digitSum = (node1 ? node1.val : 0) + (node2 ? node2.val : 0) + carry;
+
+    carry = Math.floor(digitSum / 10);
+    const currentDigit = digitSum % 10;
+
+    const newNode = new ListNode(currentDigit);
+    currentNode.next = newNode;
+    currentNode = newNode;
+
+    if (node1) {
+      node1 = node1.next;
+    }
+    if (node2) {
+      node2 = node2.next;
+    }
+  }
+
+  return dummyHead.next;
+};
